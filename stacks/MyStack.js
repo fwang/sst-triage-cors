00ff8234080794b4c5dfcce6b865ecaa9f60e0dd
Frank <wangfanjie@gmail.com>
2022-03-23 18:19:19 -0400

    Initial commit

---
+++ stacks/MyStack.js
@@ -0,0 +1,25 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class MyStack extends sst.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // Create a HTTP API
+    const api = new sst.Api(this, "Api", {
+      cors: {
+        allowOrigins: [
+          "http://localhost:3000",
+          "https://resttesttest.com",
+        ],
+      },
+      routes: {
+        "GET /": "src/lambda.handler",
+      },
+    });
+
+    // Show the endpoint in the output
+    this.addOutputs({
+      "ApiEndpoint": api.url,
+    });
+  }
+}
